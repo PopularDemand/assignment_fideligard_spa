a9f52843549f1bdfd762edc61f84da0af2f9782a
Jessica G <gillan.jessica@gmail.com>
2017-01-27 10:49:11 -0700

    route skeleton and notes

---
+++ js/app.js
@@ -0,0 +1,45 @@
+var fideligard = angular.module('fideligard', ['ui.router'])
+
+fideligard.config(['$stateProvider', '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('portfolio');
+
+    $stateProvider
+    .state('index', {
+      url: '/',
+      abstract: true,
+      views: {
+        'date-picker@': {
+          template: 'date panel'
+          // templateUrl: 'js/templates/date-picker.html'
+        },
+        'stock-panel@': {
+          template: 'stock panel'
+          // templateUrl: 'js/templates/stock-panel.html'
+        }
+      }
+    })
+    .state('index.portfolio', {
+      url: 'portfolio',
+      views: {
+        'main-panel@': {
+          template: 'portfolio main panel'
+        }
+      }})
+      .state('index.transactions', {
+        url: 'transactions',
+        views: {
+          'main-panel@': {
+            template: 'transactions main panel'
+          },
+        }
+      })
+      .state('index.trade', {
+        url: 'trade',
+        views: {
+          'main-panel@': {
+            template: 'trade main panel'
+          },
+        }
+      })
+}]);
