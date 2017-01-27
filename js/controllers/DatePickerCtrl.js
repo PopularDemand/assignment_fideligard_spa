bb1e349414212ca42167ccd2055708b640e19370
Jessica G <gillan.jessica@gmail.com>
2017-01-27 15:24:25 -0700

    date calculations

--- js/controllers/DatePickerCtrl.js
+++ js/controllers/DatePickerCtrl.js
@@ -1,7 +1,7 @@
-fideligard.controller('DatePickerCtrl', ['$scope', function($scope){
+fideligard.controller('DatePickerCtrl', ['$scope', 'dateService', function($scope, dateService){
   var _addEventListeners = function addEventListeners() {
     var $dp = $('#date-picker');
-    $dp.on('input', function(e) { 
+    $dp.on('input', function(e) {
       $('#date-label').css({ left: (e.target.clientWidth - 15) / 364 * e.target.value })
     })
   }
---
+++ js/services/dateService.js
@@ -0,0 +1,32 @@
+fideligard.factory('dateService', function() {
+  var _date = new Date('2016-01-01');
+
+  var get = function(){
+    return _date
+  }
+
+  var getDateAsStep = function(start, end, numberSteps){
+    start = start || new Date('2016-01-01');
+    end = end || new Date('2016-12-31');
+    numberSteps = numberSteps || 364;
+
+    if(_date < start) return 1;
+    if(_date > end) return numberSteps + 1;
+
+    return Math.floor((numberSteps/(end - start)) * ( _date - start))
+  }
+
+  var setDateFromStep = function(step, start, end, numberSteps){
+    start = start || new Date('2016-01-01');
+    end = end || new Date('2016-12-31');
+    numberSteps = numberSteps || 364;
+
+    var date = (((end - start)/numberSteps) * step) + start // math is right!
+  }
+
+  return {
+    get: get,
+    getDateAsStep: getDateAsStep,
+    setDateFromStep: setDateFromStep
+  }
+});
