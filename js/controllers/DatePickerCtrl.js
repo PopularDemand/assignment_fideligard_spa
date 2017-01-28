f9e5077f5596fc22e1a3313a39830097560c70f5
Alexa <alexa.anderson@outlook.com>
2017-01-27 23:44:45 -0600

    Working slider

--- js/controllers/DatePickerCtrl.js
+++ js/controllers/DatePickerCtrl.js
@@ -1,17 +1,16 @@
 fideligard.controller('DatePickerCtrl', ['$scope', 'dateService', function($scope, dateService){
-  var _addEventListeners = function addEventListeners() {
+  var _addEventListeners = function() {
     var $dp = $('#date-picker');
     $dp.on('input', function(e) {
-      $('#date-label').css({ left: (e.target.clientWidth - 15) / 364 * e.target.value })
-      dateService.setDateFromStep(e.target.value);
-      console.log('here')
+      $('#date-label').css({ left: (e.target.clientWidth - 45) / 364 * $scope.dateValue})
+      dateService.setDateFromStep($scope.dateValue);
     });
   };
 
   var _initializeValues =function() {
     $dp = $('#date-picker');
-    $dp.val(dateService.getDateAsStep());
-    $('#date-label').css({ left: ($dp.clientWidth - 15) / 364 * $dp.value })
+    $scope.dateValue = dateService.getDateAsStep();
+    $('#date-label').css({ left: ($dp.clientWidth - 45) / 364 * $dp.value })
   }
 
   $scope.getScope = function() {
