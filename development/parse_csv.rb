4b170ded8ac24e8e629b3264cf9278b2ef087828
Alexa <alexa.anderson@outlook.com>
2017-01-31 09:39:29 -0600

    Clean up file structure

--- development/parse_csv.rb
+++
@@ -1,9 +0,0 @@
-require 'csv'
-require 'json'
-
-body = File.read('/home/alexa/Desktop/viking/unit-5/fideligard/development/data.csv')
-
-csv = CSV.new(body, :headers => true, :header_converters => :symbol, :converters => :all)
-data = csv.to_a.map {|row| row.to_hash }
-
-File.open('data.json', 'w+') { |f| f.write data.to_json }
