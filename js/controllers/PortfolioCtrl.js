f4698f065de8d598f7e85c5dbaa02932e62d9182
Alexa <alexa.anderson@outlook.com>
2017-01-31 11:12:54 -0600

    refactor portfolio

--- js/controllers/PortfolioCtrl.js
+++ js/controllers/PortfolioCtrl.js
@@ -1,5 +1,6 @@
 fideligard.controller('PortfolioCtrl', ['$scope', 'transactionService', function($scope, transactionService) {
   $scope.selectedContent = 'portfolio';
+  $scope.portfolioStats = {};
 
   $scope.updateTransactions = function() {
     $scope.transactions = transactionService.getInfo();
@@ -9,4 +10,40 @@ fideligard.controller('PortfolioCtrl', ['$scope', 'transactionService', function
   $scope.$watch('transactions.all', function(oldVal, newVal, scope) {
     $scope.updateTransactions();
   }, true);
+
+  $scope.populateTables = function() {
+    $scope.portfolioStats.cash = 123456;
+    $scope.portfolioStats.value = 456789;
+    $scope.portfolioStats.profit = 123456;
+    $scope.portfolioStats.dayProfit = -123;
+    $scope.portfolioStats.weekProfit = -456;
+    $scope.portfolioStats.monthProfit = 12345;
+
+    $scope.transactions.all.push({
+          date: new Date(),
+          id: 1,
+          price: 64.34,
+          quantity: 2,
+          symbol: 'AAL',
+          type: 'buy'
+    })
+
+    $scope.transactions.all.push({
+          date: new Date(),
+          id: 2,
+          price: 64.34,
+          quantity: 2,
+          symbol: 'AAL',
+          type: 'buy'
+    })
+
+    $scope.transactions.all.push({
+          date: new Date(),
+          id: 3,
+          price: 234.44,
+          quantity: 43,
+          symbol: 'GMO',
+          type: 'buy'
+    })
+  }
 }])
--- js/directives/tradeFormDirective.js
+++ js/directives/tradeFormDirective.js
@@ -14,8 +14,9 @@ fideligard.directive('tradeForm', ['$state', 'transactionService', function($sta
           return Number(scope.stock.Close) * scope.trade.quantity;
       }
       scope.placeOrder = function() {
+        tradeDate = scope.dateInfo.date.getTime();
         if (scope.form.$valid) {
-          scope.trade.date = scope.dateInfo.date;
+          scope.trade.date = tradeDate;
           scope.trade.symbol = scope.stock.Symbol;
           scope.trade.price = scope.calculateCost();
           transactionService.addTransaction(scope.trade);
